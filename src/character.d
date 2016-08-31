b1b55ee94a36b33a2cc1620f6cfbd1c5cb110965
Justin R. St-Amant <jstamant24@gmail.com>
2016-08-30 22:57:47 -0500

    Added .d files to .gitignore

--- src/character.d
+++
@@ -1,2 +0,0 @@
-src/character.o src/character.d: src/character.cpp include/character.h \
- include/drawable.h include/defines.h include/drawable.h
--- src/subject.d
+++
@@ -1,2 +0,0 @@
-src/subject.o src/subject.d: src/subject.cpp include/drawable.h \
- include/subject.h include/observer.h
